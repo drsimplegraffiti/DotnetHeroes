0f1a1b3bc682c957104baf4305a5478d1ede7df6
Ogunnusi Abayomi Joseph <70065792+drsimplegraffiti@users.noreply.github.com>
2023-12-05 01:54:14 +0100

    intiial commit

---
+++ DotnetHeroes.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace DotnetHeroes;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            Id = 2,
+            Name = "James"
+        };
+
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Id);
+        Console.WriteLine(person.DateCreated);
+
+        Console.WriteLine($"my name is {person.Name} and my id is {person.Id}");
+    }
+
+    class Person : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+
+    }
+
+    class Animal : BaseEntity
+    {
+        public string Color { get; set; } = string.Empty;
+    }
+
+    class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated = DateTime.UtcNow;
+    }
+}
